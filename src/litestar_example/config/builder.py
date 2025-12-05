f7eae4c374c21309247ac18cfbcea0234c9a84e7
Sebastian Pietras <github@mail.spietras.dev>
2025-12-06 00:41:44 +0100

    Added template files (#48)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/litestar_example/config/builder.py
@@ -0,0 +1,15 @@
+from pydantic import ValidationError
+
+from litestar_example.config.errors import ConfigError
+from litestar_example.config.models import Config
+
+
+class ConfigBuilder:
+    """Builds the config."""
+
+    def build(self) -> Config:
+        """Build the config."""
+        try:
+            return Config()
+        except ValidationError as ex:
+            raise ConfigError from ex
---
+++ src/litestar_example/models/events/event.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+
+from pydantic import Field, RootModel
+
+from litestar_example.models.events import bar as be
+from litestar_example.models.events import foo as fe
+
+type Event = Annotated[be.BarEvent | fe.FooEvent, Field(discriminator="type")]
+ParsableEvent = RootModel[Event]
